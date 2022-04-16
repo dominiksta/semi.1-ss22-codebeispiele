b9e005650d54b9c420c7e14a251bd2b16038c907
dominiksta <dominik.stahmer@posteo.de>
2022-04-16 15:15:27 +0200

    feat: anzahl vorkommen schlüsselwörter anstatt wahrheitswert

--- sgd.py
+++ sgd.py
@@ -18,16 +18,16 @@ Frequency – Inverse Document Frequency) ermittelt.
 # einsetzbaren Classifier zu trainieren.)
 training_data = [
     {
-        'keywords': {
-            'cloud'  : True,  'performance': True,  'scalable': True,
-            'connect': False, 'coverage'   : False, 'service' : False,
+        'keywords_count': {
+            'cloud'  : 5,  'performance': 4,  'scalable': 5,
+            'connect': 0,  'coverage'   : 0,  'service' : 1,
         },
         'is_isp': True
     },
     {
-        'keywords': {
-            'cloud'  : False, 'performance': False, 'scalable': False,
-            'connect': True,  'coverage'   : True,  'service' : True,
+        'keywords_count': {
+            'cloud'  : 0,  'performance': 1,  'scalable': 0,
+            'connect': 5,  'coverage'   : 5,  'service' : 4,
         },
         'is_isp': False
     }
@@ -35,17 +35,17 @@ training_data = [
 
 # Die Daten müssen zur Verwendung mit sklearn noch etwas transformiert werden.
 #
-# X ist hierbei eine Liste von Listen mit True/False Werten zu den einzelnen
-# Schlüsselwörtern:
+# X ist hierbei eine Liste von Listen mit der Anzahl der Vorkommen der einzelnen
+# Schlüsselwörter:
 # [
-#     [True, False, True, ...],
-#     [False, False, True, ...],
+#     [5, 4, 5, 0, 0, 1],
+#     [0, 1, 0, 5, 5, 4],
 #     ...
 # ]
 #
 # y ist eine Liste der is_isp Werte: [True, False, ...]
 
-X = list(map(lambda el: list(el['keywords'].values()), training_data))
+X = list(map(lambda el: list(el['keywords_count'].values()), training_data))
 y = list(map(lambda el: el['is_isp'], training_data))
 
 
@@ -59,11 +59,17 @@ y = list(map(lambda el: el['is_isp'], training_data))
 sgd_classifier = SGDClassifier() # Classifier Objekt initialisieren
 sgd_classifier.fit(X, y)         # --- Classifier trainieren ---
 
-to_predict = np.array(
-    list({
-        'cloud'  : False, 'performance': False, 'scalable': False,
-        'connect': True,  'coverage'   : True,  'service' : False,
-    }.values())
-).reshape(1, -1)
+to_predict_1 = {
+    'cloud'  : 5,  'performance': 3,  'scalable': 4,
+    'connect': 1,  'coverage'   : 1,  'service' : 0,
+}
+to_predict_2 = {
+    'cloud'  : 5,  'performance': 3,  'scalable': 4,
+    'connect': 20,  'coverage'   : 20,  'service' : 20,
+}
 
-print(sgd_classifier.predict(to_predict))
+to_predict_1 = np.array(list(to_predict_1.values())).reshape(1, -1)
+to_predict_2 = np.array(list(to_predict_2.values())).reshape(1, -1)
+
+print(sgd_classifier.predict(to_predict_1)) # -> True
+print(sgd_classifier.predict(to_predict_2)) # -> False
